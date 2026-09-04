2bcacd663f2a84e8eca1cbd93018d3c6021f0a3a
Wurielle <noahfenghom@gmail.com>
2026-09-04 09:54:57 +0200

    fix: fix curl error with steam default runtime

--- recipes/reframework/index.ts
+++ recipes/reframework/index.ts
@@ -6,7 +6,8 @@ const repositoryName = '{{env:RECIPE_REPOSITORY_NAME}}'
 const commitSha = '{{env:RECIPE_COMMIT_SHA}}'
 const scriptsBaseUrl = `https://raw.githubusercontent.com/${repositoryOwner}/${repositoryName}/${commitSha}/recipes/reframework/scripts`
 const wineDllOverrides = 'WINEDLLOVERRIDES="dinput8.dll=n,b"'
-const runScript = (scriptName: string) => `bash -c 'curl -fsSL --retry 3 --retry-delay 1 "${scriptsBaseUrl}/${scriptName}" | bash -s -- "$STEAM_COMPAT_INSTALL_PATH"; exec "$@"' -- %command%`
+const hostRuntime = '"${STEAM_RUNTIME}/scripts/switch-runtime.sh" --runtime="" --'
+const runScript = (scriptName: string) => `bash -c '${hostRuntime} curl -fsSL --retry 3 --retry-delay 1 "${scriptsBaseUrl}/${scriptName}" | bash -s -- "$STEAM_COMPAT_INSTALL_PATH"; exec "$@"' -- %command%`
 
 const actionValues = [
     {
